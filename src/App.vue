0e73175d56889a79366639ffe5b90155d43e41c8
A-A-Z <dramatichamster@gmail.com>
2018-02-16 00:09:27 +1100

    Added basics

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,104 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="logo-preview">
+
+      <div class="label-width">{{ (millimetres(logoSize, logoMetric) ) / 10 }} CM</div>
+      <div class="label-height">{{ (millimetres(logoSize, logoMetric) ) / 10 }} CM</div>
+
+      <div class="logo"></div>
+
+    </div>
+
+    <div class="settings">
+      <input type="number" v-model.number="logoSize">
+      <select v-model.number="logoMetric">
+        <option v-for="(metric, index) in metrics" :value="index" :key="metric.value">{{ metric.short }}</option>
+      </select>
+    </div>
+
+    <div class="output">
+      output: ({{ surface }})
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const data ={
+  logoSize: 1,
+  logoMetric: 1,
+  metrics: [
+    { long: 'millimetres', short: 'mm', value: 1 },
+    { long: 'centimetres', short: 'cm', value: (1 * 10) },
+    { long: 'metres', short: 'm', value: (1 * 10 * 100) }
+  ],
+  pi: 3.14159265359
+}
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+
+  data: () => {
+    return data
+  },
+
+  beforeMount () {
+    // console.log(this.millimetres(this.logoSize, 1))
+  },
+
+  methods: {
+    millimetres(size, metric) {
+      return size * this.metrics[metric].value
+    },
+
+    getSurface (width) {
+      const radius = width / 2
+      const circleSurface = radius * radius * Math.PI
+      const squireSurface = width * width
+      return (circleSurface * 0.75) + (squireSurface * 0.25)
+    }
+  },
+
+  computed: {
+    surface () {
+      return this.getSurface(this.millimetres(this.logoSize, this.logoMetric))
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+// colours
+$colour-logo: #FFF;
+$colour-logo-bg: #444;
+
+// sizes
+$logo-size: 200px;
+
+body {
+
+}
+
+.logo-preview {
+  background-color: $colour-logo-bg;
+  color: $colour-logo;
+  padding: 50px;
+
+  .logo {
+    background-color: $colour-logo;
+    border-radius: 50% 50% 0 50%;
+    height: $logo-size;
+    width: $logo-size;
+  }
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
